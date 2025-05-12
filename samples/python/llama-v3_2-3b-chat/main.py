472b1d7e96905464b900c9748648ab4dc2fefbb8
Yung-Hsiang Hu <iftnt1999@gmail.com>
2025-05-12 19:59:42 +0800

    add: sample of llama-v3.2-3b-chat

---
+++ samples/python/llama-v3_2-3b-chat/main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import time
+from importlib.metadata import version
+from huggingface_hub import snapshot_download
+
+# import dlltracer
+# with dlltracer.Trace(out=sys.stdout):
+from qai_appbuilder.geniecontext import GenieContext
+
+def callback(result):
+    global g_count
+    print(f"[Callback] {result}", flush=True)
+    return False # Return true to terminate the generation
+
+def main():
+    print(f"PID={os.getpid()}")
+    print("Version of qai_appbuilder=" + version("qai_appbuilder"))
+
+    model_dir = snapshot_download(repo_id="thuniverse-ai/Llama-v3.2-3B-Chat-GENIE")
+    os.chdir(model_dir)
+    model = GenieContext("genie_config.json")
+    print("model loaded")
+    prompt = "<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\nIntroduce yourself.<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+    print("[Begin inference]")
+    model.Query(prompt=prompt, callback=callback)
+    print("[End inference]")
+
+    sys.stdout.flush()
+
+if __name__ == "__main__":
+    main()
